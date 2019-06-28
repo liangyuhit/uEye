bceb3036d7c9514b8e176f0a3c40c538d7f550c3
liangyuhit <312592076@qq.com>
2019-06-28 13:58:40 +0200

    2019-6-28 13:58:39

--- test.py
+++ test.py
@@ -4,3 +4,8 @@ Created on 28.06.2019
 
 @author: yu03
 '''
+# from pyueye_example_gui import PyuEyeQtApp, PyuEyeQtView
+import pyueye
+# import pyueye_example_gui
+import PyQt5
+from PyQt5.QtWidgets import QGraphicsScene, QApplication, QWidget
